a8e9f365e0ebcb28b4d8e6c8a52a1089d9b12266
HTMLissa <melissa_w1994@yahoo.de>
2022-11-11 12:30:50 -1000

    adding player symbols and iffy to create new players

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+// factory function to create new players
+const Player = (name, sign) => {
+  return { name, sign };
+};
+
 // function to store the gameboard as an array inside of a Gameboard object
-const Gameboard = (function () {
-  let _gameboardArr = ["", "", "", "", "", "", "", "", ""];
-  const getGameboard = () => {
-    console.log(_gameboardArr);
+const gameboard = (function () {
+  let _board = ["X", "O", "X", "X", "", "", "", "", ""];
+  let getGameboard = () => {
+    console.log(_board);
   };
   return { getGameboard };
 })();
 
-// funtion to store each player inside of an object & all players inside of an array
-const Player = (function (name) {
-  name = this.name;
-  console.log(name);
+// function to store all gameplay relevant functions & variables
+const gamePlay = (function () {
+  // SELECTING BOARD CELLS
+  const cells = document.querySelectorAll(".cell");
+  // LOOPING OVER BOARD CELLS & ADDING EVENT LISTENERS TO EACH CELL
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      console.log("you clicked me");
+    });
+  });
+
+  // function to loop over gameboard arr & render the arr content to the webpage
+  const displayBoard = () => {
+    let _board = gameBoard.getGameboard();
+    for (let cell of cells) {
+      //
+    }
+  };
+
+  return { displayBoard };
 })();
