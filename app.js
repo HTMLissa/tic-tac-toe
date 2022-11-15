5407acd8dcaa470a108e40d12ade96fcefae4274
HTMLissa <melissa_w1994@yahoo.de>
2022-11-15 12:07:43 -1000

    adding current player's mark to the corresponding _board arr

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const gameboard = (function () {
 
 // function to store all gameplay relevant functions & variables
 const gamePlay = (function () {
+  // GETTING THE CURRENT BOARD ARR
+  let _board = gameboard.getGameboard();
   // SELECTING BOARD CELLS
   const cells = document.querySelectorAll(".cell");
   // CREATING A VARIABLE TO KEEP TRACK OF THE TURNS
@@ -25,8 +27,11 @@ const gamePlay = (function () {
   // CREATING FUNCTION TO MAKE A MOVE EACH TIME A CELL IS CLICKED
   function makeMove(e) {
     let cell = e.target;
+    let index = cell.dataset.index;
     // SET MARK
     cell.classList.add(currentPlayer);
+    // SET CORRESPONDING BOARD-ARR VALUE;
+    _board[index] = currentPlayer;
     // CHECK FOR WIN
     // CHECK FOR TIE
     // SWITCH TURNS
@@ -44,7 +49,6 @@ const gamePlay = (function () {
 
   // function to loop over gameboard arr & render the arr content to the webpage
   const displayBoard = () => {
-    let _board = gameboard.getGameboard();
     for (let i = 0; i <= cells.length; i++) {
       let cellValue = _board[i];
       if (cellValue != "") {
